3ac28f12f1dbf863324e882c91ee119063452751
Gabriel Alves <alvezinho.gabs@gmail.com>
2022-03-03 11:58:17 -0300

    Um contador

---
+++ Contador/script.js
@@ -0,0 +1,21 @@
+function solve(){
+    let getNum1 = document.getElementById('num1');
+    let getNum2 = document.getElementById('num2');
+    let getPasso = document.getElementById('passos');
+    let text = document.getElementById('text');
+    let content = document.getElementById('content');
+    
+
+    let num1 = Number(getNum1.value);
+    let num2 = Number(getNum2.value);
+    let passo = Number(getPasso.value);
+    text.innerHTML = '<strong> Resolvendo... <strong/>';
+    
+    content.innerHTML = '<strong> O resultado é: <strong/>'
+    while (num1 <= num2){
+        content.innerHTML += `${num1}👉`;
+        num1 += passo;
+        
+    } 
+    content.innerHTML += '🏴';
+}
